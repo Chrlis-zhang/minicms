e432d26d581cf7064afe1776970783f45e805f68
Administrator <bie.re.wo@163.com>
2016-09-07 17:08:40 +0800

    2016-9-7

--- news/views.py
+++ news/views.py
@@ -8,16 +8,22 @@ from news.models import Column,Article
 def index(request):
     columns = Column.objects.all()
     return render(request,'index.html',{'columns': columns})
+
+
     #return HttpResponse(u'欢迎')
 
 
 def column_detail(request,column_slug):
     cloumn = Column.objects.get(slug=column_slug)
     return render(request,'column.html',{'column':cloumn})
+
+
     #return HttpResponse('column_slug' + column_slug)
 
 
 def article_detail(request,article_slug):
     articl = Article.objects.get(slug=article_slug)
     return render(request,'article.html',{'article':articl})
+
+
     #return HttpResponse('article_slug' + article_slug)
